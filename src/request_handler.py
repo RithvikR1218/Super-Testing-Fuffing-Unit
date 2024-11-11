32870d56429fb02d46747fbed495fc866d8f90e0
NitroKnight68 <nish0602@gmail.com>
2024-11-11 09:13:18 +0530

    feat: vhost discovery

--- src/request_handler.py
+++ src/request_handler.py
@@ -1,10 +1,16 @@
 import requests
 
 def send_request(method, url, word, headers, data):
-    # Replace FUZZ in URL and data
-    target_url = url.replace("FUZZ", word)  # Replace FUZZ with full word in URL
-    if data:
-        data = {key: value.replace("FUZZ", word) for key, value in data.items()}  # Replace FUZZ in form data
+
+    # Replace FUZZ with Host as priority
+    if headers.get('Host'):
+        target_url = headers['Host'].replace("FUZZ", word)
+        target_url = 'http://' + target_url
+    else:
+        # Replace FUZZ in URL and data
+        target_url = url.replace("FUZZ", word)  # Replace FUZZ with full word in URL
+        if data:
+            data = {key: value.replace("FUZZ", word) for key, value in data.items()}  # Replace FUZZ in form data
 
     try:
         if method.upper() == "GET":
@@ -17,4 +23,4 @@ def send_request(method, url, word, headers, data):
 
         print(f"{target_url} => {response.status_code}")
     except requests.RequestException as e:
-        print(f"Error sending {method} request to {target_url}: {e}")
+        print(f"Error sending {method} request to {target_url}")
